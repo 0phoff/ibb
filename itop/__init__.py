e9c5e97e0fae66dfd35199dcbd98f87c43122397
0phoff <0phoff@users.noreply.github.com>
2019-08-06 13:20:46 +0200

    Exposed widgets

--- itop/__init__.py
+++ itop/__init__.py
@@ -1,6 +1,8 @@
 from ._version import version_info, __version__
 
-from .example import *
+from ._image_canvas import *
+from ._image_viewer import *
+from ._brambox_viewer import *
 
 def _jupyter_nbextension_paths():
     return [{
