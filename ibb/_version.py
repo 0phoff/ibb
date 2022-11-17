3562e853417b1547ba1ce99767038a1b8c668a0b
0phoff <0phoff@users.noreply.github.com>
2022-11-17 16:30:06 +0100

    Bumped version to 2.3.1

--- ibb/_version.py
+++ ibb/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) 0phoff.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (2, 3, 1, 'dev')
+version_info = (2, 3, 1)
 __version__ = '.'.join(map(str, version_info))
